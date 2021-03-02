59e3395dab09e128458f6282ab80e27f7206368d
Asher Seiling <f14asher@gmail.com>
2021-03-02 13:22:03 -0600

    Clearing function

---
+++ src/render.js
@@ -0,0 +1,20 @@
+// The variables that get_input needs to get
+let key = "";
+let master_pass = "";
+
+// Gets the input of the first page
+function get_input(){
+    key = document.getElementById("password_key").value;
+    master_pass = document.getElementById("master_pass").value;
+};
+
+// Assemble HTML
+function assemble_html(){
+    
+};
+
+// Main function
+function main(){
+    get_input();
+    $("#center").html("");
+};
